ab7112c88a69a6b05a09f9ada7b0065192662c39
Ewelina Skibinska <skibinska@users.noreply.github.com>
2017-02-24 23:27:57 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
 'use strict';
-
-function fizzbuzz (number) {
-  if (number === 5) {
-    return "Buzz";
-  }
-  else if (number === 4) {
-    return 4;
-  }
-  else if (number === 3) {
-    return "Fizz";
-  }
-  else if (number === 2) {
-    return 2;
-  }
-  return 1;
-}
